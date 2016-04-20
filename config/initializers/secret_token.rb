a147a78a568f2286828445610bb5e52a81db205f
Julia Simon <julia.solstice.simon@gmail.com>
2016-04-20 01:00:33 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brainwidth::Application.config.secret_key_base = '71ad4a3e88b9feb39d60449266e0af5aaee126b830321aff599d6c188c7094858a27453e1bf2dc9a113fcef1a79adacfb5e46c223a4465b2ebc3a432dd641f65'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Brainwidth::Application.config.session_store :cookie_store, key: '_brainwidth_session'
