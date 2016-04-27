f3d24312de8dd1850e40c36a8ecd97ed9f2673f8
Julia Simon <julia.solstice.simon@gmail.com>
2016-04-27 23:02:15 +0000

    added create function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,25 @@ class TasksController < ApplicationController
 	def new
 		@task = Task.new
 	end
+
+	def create
+		@task = Task.create(task_params)
+		if @task.valid?
+			redirect_to root_path
+		else
+			return render :edit, status: :unprocessable_entity
+		end
+	end
+
+
+
+
+
+	private
+
+	def task_params
+		params.require(:task).permit(:title, :description, :date, :value, :category)
+	end
+
+
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -8,10 +8,20 @@ RSpec.describe TasksController, type: :controller do
 		end
 	end
 
-	describe "grams#new action" do
+	describe "tasks#new action" do
 		it "should successfully show the new form" do
 			get :new
 			expect(response).to have_http_status(:success)
+		end
+	end
+
+	describe "tasks#create action" do
+		it "should successfully create a new task in the database" do
+			post :create, task: {title: 'Hello!'}
+			expect(response).to redirect_to root_path
+
+			task = Task.last
+			expect(task.title).to eq('Hello!')
 		end
 	end
 
