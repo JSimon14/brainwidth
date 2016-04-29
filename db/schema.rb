2231754397de610f4db2d0242354d64864535050
Julia Simon <julia.solstice.simon@gmail.com>
2016-04-29 16:53:29 +0000

    added user log-in requirements

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427231039) do
+ActiveRecord::Schema.define(version: 20160429162153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,10 @@ ActiveRecord::Schema.define(version: 20160427231039) do
     t.string   "category"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -9,25 +9,59 @@ RSpec.describe TasksController, type: :controller do
 	end
 
 	describe "tasks#new action" do
+		it "should require users tp be logged in" do
+			get :new
+			expect(response).to redirect_to new_user_session_path
+		end
+
 		it "should successfully show the new form" do
+			user = User.create(
+				email: 'fakeemail@yahoo.com',
+				password: 'fakepassword',
+				password_confirmation: 'fakepassword'
+				)
+			sign_in user
+
 			get :new
 			expect(response).to have_http_status(:success)
 		end
 	end
 
 	describe "tasks#create action" do
+		it "should require users tp be logged in" do
+			post :create, task: {title: 'Hello!', date: 'April 27 2016', value: '2', category: 'work'}
+			expect(response).to redirect_to new_user_session_path
+		end
+
+
 		it "should successfully create a new task in the database" do
+			user = User.create(
+				email: 'fakeemail@yahoo.com',
+				password: 'fakepassword',
+				password_confirmation: 'fakepassword'
+				)
+			sign_in user
+
 			post :create, task: {title: 'Hello!', date: 'April 27 2016', value: '2', category: 'work'}
 			expect(response).to redirect_to root_path
 
 			task = Task.last
 			expect(task.title).to eq('Hello!')
+			expect(task.user).to eq(user)
 		end
 
 		it "should properly deal with validation errors" do
+			user = User.create(
+				email: 'fakeemail@yahoo.com',
+				password: 'fakepassword',
+				password_confirmation: 'fakepassword'
+				)
+			sign_in user
+
+			task_count = Task.count
 			post :create, task: {title: '', date: '', value: '', category: '' }
 			expect(response).to have_http_status(:unprocessable_entity)
-			expect(Task.count).to eq 0 
+			expect(task_count).to eq Task.count
 		end
 	end
 
